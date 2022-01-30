d56099a9e491bc3ed508d7c03b201595c0759cf9
superMario737 <82049062+mariuslisinskas@users.noreply.github.com>
2022-01-30 18:36:53 +0200

    Updatint data.service

--- src/app/darbai/darbai.component.ts
+++ src/app/darbai/darbai.component.ts
@@ -10,28 +10,12 @@ import { DataService } from '../data.service';
 
 export class DarbaiComponent implements OnInit {
 
-    isActive:boolean = true;
     public paslaugos;
-    public ak:number=0;
-    public ilgis:number=0;
-    public k:number;
-
-    status(i){
-        console.log(i)
-        if(this.isActive==false){
-            this.isActive=true;
-        } else{
-            this.isActive=false; 
-        }
-        
-        console.log();
-    }
 
   constructor(public dataService : DataService) { }
 
   ngOnInit(): void {
     this.paslaugos = this.dataService.paslaugos;
-    this.ilgis = this.paslaugos.length;
   }
 
 }
